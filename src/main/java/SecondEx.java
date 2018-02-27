a0985517531d2e43b51b4e934d26ea5225faaace
PixarV <pixarvv@yandex.ru>
2018-02-27 00:57:41 +0000

    First commit // 2nd ex

---
+++ src/main/java/SecondEx.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import static java.lang.Math.pow;
+
+public class SecondEx {
+        public static void main(String... args) {
+                Scanner scanner = new Scanner(System.in);
+                double eps = scanner.nextDouble();
+
+                double a = 1; // n = 0
+                int n = 1;
+
+                while (a  > eps) {
+                        a = 1 / pow(n + 1, 2);
+                        System.out.println(a);
+                        n++;
+                }
+                System.out.println(--n);
+        }
+}
